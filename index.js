9597734327ead5f59699879c5af1fcb9c519da8c
jeancy001 <jeancympoy24@gmail.com>
2025-03-25 06:25:59 -0400

    added subscribe

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
+    origin: "*", 
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"], 
     allowedHeaders: ["Content-Type", "Authorization"],
